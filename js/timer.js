af970007cfd038d94e193de2e20952c4db47bc42
Tatsu-tatsu <tatsu.progra7@gmail.com>
2020-08-16 11:23:43 +0900

    timer.jsの追加

---
+++ js/timer.js
@@ -0,0 +1,13 @@
+var end = function() {
+
+};
+
+var starttime = Date.now();
+var nowtime = Date.now();
+var endtime = starttime + 60000;
+
+var lasttime = document.getElementById("").value;
+
+
+setTimeout(end, 60000);//60秒後にendを実行
+
